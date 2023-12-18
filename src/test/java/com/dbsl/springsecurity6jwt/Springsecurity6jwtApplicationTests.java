7a6bd2fa8bbad4b3d562feb75e1d1ac7a6d4327f
Diego-pc <dleon.desarrollo@gmail.com>
2023-12-18 01:50:11 -0300

    Subiendo proyecto con librerias principales

---
+++ src/test/java/com/dbsl/springsecurity6jwt/Springsecurity6jwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbsl.springsecurity6jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecurity6jwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
