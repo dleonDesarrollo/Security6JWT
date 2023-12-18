43a8f3d85c0085ab9353a2f0d2a803757dea2384
Diego-pc <dleon.desarrollo@gmail.com>
2023-12-18 16:02:02 -0300

    aplicando anotacion a los repository y uso de JPA, se definen funciones en APIRest ProductController

--- src/main/java/com/dbsl/springsecurity6jwt/controllers/ProductController.java
+++ src/main/java/com/dbsl/springsecurity6jwt/controllers/ProductController.java
@@ -1,4 +1,37 @@
 package com.dbsl.springsecurity6jwt.controllers;
 
+import com.dbsl.springsecurity6jwt.entities.Product;
+import com.dbsl.springsecurity6jwt.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll(){
+        List<Product> products = productRepository.findAll();
+        if (!products.isEmpty()){
+            return ResponseEntity.ok(products);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @PostMapping
+    public ResponseEntity<Product> createOne(@RequestBody @Valid Product product){
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(product));
+    }
 }
--- src/main/java/com/dbsl/springsecurity6jwt/repositories/UserRepository.java
+++ src/main/java/com/dbsl/springsecurity6jwt/repositories/UserRepository.java
@@ -1,4 +1,9 @@
 package com.dbsl.springsecurity6jwt.repositories;
 
-public interface UserRepository {
+import com.dbsl.springsecurity6jwt.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
