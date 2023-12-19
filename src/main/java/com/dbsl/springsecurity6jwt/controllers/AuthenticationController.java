4941a6710c6829a875ab0e0883ccd8cb6c6ed417
Diego-pc <dleon.desarrollo@gmail.com>
2023-12-18 23:54:32 -0300

    Definiendo rutas de AuthenticationController y creando los DTO para manejar las peticiones del cliente

--- src/main/java/com/dbsl/springsecurity6jwt/controllers/AuthenticationController.java
+++ src/main/java/com/dbsl/springsecurity6jwt/controllers/AuthenticationController.java
@@ -1,9 +1,19 @@
 package com.dbsl.springsecurity6jwt.controllers;
 
+import com.dbsl.springsecurity6jwt.dto.AuthenticationRequest;
+import com.dbsl.springsecurity6jwt.dto.AuthenticationResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody @Valid AuthenticationRequest authRequest){
+        return null;
+    }
 }
---
+++ src/main/java/com/dbsl/springsecurity6jwt/dto/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.dbsl.springsecurity6jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class AuthenticationResponse {
+    private String jwt;
+
+}
